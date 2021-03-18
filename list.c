b3ef2b440bbac63f1b19f3ed1867f292ad36f8d7
Replit user <replituser@example.com>
2021-03-18 22:36:27 +0000

    new try

--- list.c
+++ list.c
@@ -6,57 +6,89 @@
 typedef struct Node Node;
 
 struct Node {
-    const void * data;
-    Node * next;
-    Node * prev;
+  const void * data;
+  Node * next;
+  Node * prev;
 };
 
 struct List {
-    Node * head;
-    Node * tail;
-    Node * current;
+  Node * head;
+  Node * tail;
+  Node * current;
 };
 
 typedef List List;
 
 Node * createNode(const void * data) {
-    Node * new = (Node *)malloc(sizeof(Node));
-    assert(new != NULL);
-    new->data = data;
-    new->prev = NULL;
-    new->next = NULL;
-    return new;
+  Node * new_node = (Node *)calloc(1, sizeof(Node));
+  assert(new_node);
+  if (!new_node) { return(NULL); }
+  new_node->data = data;
+  return(new_node);
 }
 
 List * createList() {
-     return NULL;
+  List* new_list = calloc(1, sizeof(List));
+  assert(new_list);
+  if (!new_list) { return(NULL); }
+  return(new_list);
 }
 
 void * firstList(List * list) {
-    return NULL;
+  list->current = list->head;
+  return(list->current);
 }
 
 void * nextList(List * list) {
-    return NULL;
+  list->current = list->current->next;
+  return(list->current);
 }
 
 void * lastList(List * list) {
-    return NULL;
+  list->current = list->tail;
+  return(list->current);
 }
 
 void * prevList(List * list) {
-    return NULL;
+  list->current = list->current->prev;
+  return(list->current);
 }
 
 void pushFront(List * list, const void * data) {
+  Node* new_node = createNode(data);
+  if (list->head)
+  {
+    list->head->prev = new_node;
+    new_node->next = list->head;
+  }
+  else
+  {
+    // There is no head, so the list is empty.
+    list->head = new_node;
+    list->current = new_node;
+  }
+  list->head = new_node;
 }
 
 void pushBack(List * list, const void * data) {
-    list->current = list->tail;
-    pushCurrent(list,data);
+  list->current = list->tail;
+  pushCurrent(list, data);
 }
 
 void pushCurrent(List * list, const void * data) {
+  if (list->current)
+  {
+    Node* new_node = createNode(data);
+    Node* temp = list->current->next;
+    list->current->next = new_node;
+    new_node->next = temp;
+    if (temp) { temp->prev = new_node; }
+  }
+  else
+  {
+    // Empty?
+    pushFront(list, data);
+  }
 }
 
 void * popFront(List * list) {
@@ -70,7 +102,16 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
-    return NULL;
+  Node* temp = list->current;
+  Node* prev = temp->prev;
+  Node* next = temp->next;
+
+  if (prev) { prev->next = next; }
+  if (next) { next->prev = prev; }
+
+  const void* temp_data = temp->data;
+  free(temp);
+  return((void*)temp_data); // Fix the declaration! const void*
 }
 
 void cleanList(List * list) {
